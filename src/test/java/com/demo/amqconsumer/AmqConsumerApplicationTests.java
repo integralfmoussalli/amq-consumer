d0cc449ed0822346fd28ec8699ace94844a01e57
fmoussalli <fmoussalli@integral.com.au>
2022-04-05 14:08:04 +1000

    Working test for processor.

--- src/test/java/com/demo/amqconsumer/AmqConsumerApplicationTests.java
+++ src/test/java/com/demo/amqconsumer/AmqConsumerApplicationTests.java
@@ -1,14 +1,19 @@
 package com.demo.amqconsumer;
 
+import com.demo.amqconsumer.model.SampleMessage;
 import com.demo.amqconsumer.processor.SampleResponseProcessor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.*;
 import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.engine.DefaultProducerTemplate;
+import org.apache.camel.support.DefaultExchange;
 import org.apache.commons.io.FileUtils;
 import org.json.JSONException;
-import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,9 +25,7 @@ import org.springframework.test.context.event.annotation.BeforeTestClass;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
-import static org.apache.camel.builder.Builder.constant;
-
-@CamelSpringBootTest
+@Slf4j
 @EnableAutoConfiguration
 @SpringBootTest
 class AmqConsumerApplicationTests {
@@ -31,12 +34,9 @@ class AmqConsumerApplicationTests {
 	void contextLoads() {
 	}
 
+	ProducerTemplate producerTemplate;
 
-	@Autowired
-	ConsumerTemplate consumerTemplate;
-
-	@Autowired
-	private CamelContext camelContext;
+	CamelContext camelContext = new DefaultCamelContext();
 
 	@Autowired
 	SampleResponseProcessor sampleResponseProcessor;
@@ -50,28 +50,40 @@ class AmqConsumerApplicationTests {
 	@Value("classpath:sampleMessage.json")
 	Resource testMessage;
 
+	String sampleMessage;
+	String sampleResponse;
 
-	@BeforeTestClass
-	public void setConsumerTemplate() throws IOException {
+	JacksonDataFormat stringFormat = new JacksonDataFormat(SampleMessage.class);
 
-		String message = FileUtils.readFileToString(testMessage.getFile(), Charset.defaultCharset());
 
-		RouteBuilder routeBuilder = new RouteBuilder() {
+	@BeforeEach
+	public void setUp() throws Exception {
+
+		producerTemplate = camelContext.createProducerTemplate();
+
+		sampleResponse = FileUtils.readFileToString(testResponse.getFile(), Charset.defaultCharset());
+		sampleMessage = FileUtils.readFileToString(testMessage.getFile(), Charset.defaultCharset());
+
+		camelContext.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				from("activemq:queue:sendMessageQueue")
-						.setBody(constant(message))
+				from("direct:yeehaw")
+						.unmarshal(stringFormat)
 						.process(sampleResponseProcessor)
 						.to(mockEndpoint)
 						.end();
 			}
-		};
+		});
+
 	}
 
 	@Test
 	public void sampleResponseProcessorTest() throws IOException, JSONException {
-		consumerTemplate.receiveBody("activemq:queue:sendMessageQueue",10);
+
+		camelContext.start();
+		producerTemplate.sendBody("direct:yeehaw", sampleMessage);
 		mockEndpoint.expectedBodiesReceived(1);
+		camelContext.stop();
 	}
 
 
